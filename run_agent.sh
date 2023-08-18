9ef9c7c2c63dc45fb68feecb95da12b47ddbc58b
Paul <pseward@upei.ca>
2023-08-18 11:11:50 -0300

    chore: add more time to batch

--- run_agent.sh
+++ run_agent.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=2         # CPU cores/threads
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --gpus-per-node=1
-#SBATCH --time=40:00:00
+#SBATCH --time=90:00:00
 #SBATCH --job-name=d30f19_PSORL
 #SBATCH --output=%x-%j.out
 source ~/scratch/TF_RL/bin/activate
