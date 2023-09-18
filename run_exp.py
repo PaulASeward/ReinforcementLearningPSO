da1a21aaefdec3ea246a06e948dd00e2acb67ccc
Paul <pseward@upei.ca>
2023-09-18 20:04:36 -0300

    chore: updated run params

--- run_exp.py
+++ run_exp.py
@@ -55,15 +55,15 @@ figure_file_right_action = os.path.join(results_dir, f"{experiment}_right_action
 
 
 # Execution parameters
-num_iterations = 100
+num_iterations = 20000
 initial_collect_steps = 100
 collect_steps_per_iteration = 1
 replay_buffer_max_length = 100000
 batch_size = 64
 learning_rate = 1e-3
-log_interval = 20
+log_interval = 200
 num_eval_episodes = 10
-eval_interval = 50
+eval_interval = 500
 iterations = range(0, num_iterations + 1, eval_interval)
 
 # Creating environments
