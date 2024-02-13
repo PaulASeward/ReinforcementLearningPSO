658491f591dc2d07c69a34e2a096429a09e5d035
Paul <pseward@upei.ca>
2024-02-13 14:45:15 -0400

    rename script

--- run_agent.sh
+++ run_agent.sh
@@ -3,8 +3,8 @@
 #SBATCH --cpus-per-task=2         # CPU cores/threads
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --gpus-per-node=1
-#SBATCH --time=90:00:00
-#SBATCH --job-name=d30f19_PSORL
+#SBATCH --time=199:00:00
+#SBATCH --job-name=PSO_RL_Test_FE_Distributions
 #SBATCH --output=%x-%j.out
 source ~/scratch/TF_RL2/bin/activate
-python run_exp.py
+python main.py
