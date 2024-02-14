390825b3647145b1de610af3b70c6694f5d48c62
Paul <pseward@upei.ca>
2024-02-14 12:14:45 -0400

    update config params

--- run_agent.sh
+++ run_agent.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --gpus-per-node=1
 #SBATCH --time=199:00:00
-#SBATCH --job-name=PSO_RL_Test_FE_Distributions
+#SBATCH --job-name=Test_Actions_5_6
 #SBATCH --output=%x-%j.out
 source ~/scratch/TF_RL2/bin/activate
 python main.py --network_type=DRQN --func_num=19 --num_actions=5 --num_episodes=10 --num_swarm_obs_intervals=10 --swarm_obs_interval_length=60
