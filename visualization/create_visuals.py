ca10f88dbf78b6cb5510fee3fcf75a476cc83615
Paul <pseward@upei.ca>
2024-04-08 11:54:55 -0300

    created mp3 visual tool

--- visualization/create_visuals.py
+++ visualization/create_visuals.py
@@ -1,7 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+
+import matplotlib.pyplot as plt
+from matplotlib.markers import MarkerStyle
 from matplotlib import animation
+from matplotlib.animation import FFMpegWriter, FuncAnimation
+import warnings
+warnings.filterwarnings('ignore')
 import environment.functions as functions
 
 # Load your data
@@ -24,40 +30,49 @@ def eval(X):
     """
     return obj_f.Y_matrix(np.array(X).astype(float))
 
-# Create a meshgrid for the background
-x1 = np.linspace(-100, 100, 100)
-x2 = np.linspace(-100, 100, 100)
 
-X1, X2 = np.meshgrid(x1, x2)
-points = np.stack([X1.ravel(), X2.ravel()], axis=-1)
-Z_flat = eval(points)
-Z = Z_flat.reshape(X1.shape)
+def animate_episode(positions, valuations, animation_path='data/animation.mp4'):
+    plt.close()
+    # Create a meshgrid for the background
+    x1 = np.linspace(-100, 100, 100)
+    x2 = np.linspace(-100, 100, 100)
+    X1, X2 = np.meshgrid(x1, x2)
 
-# X = np.stack([x1, x2], axis=-1)
-# Z = eval(X)
+    points = np.stack([X1.ravel(), X2.ravel()], axis=-1)
+    Z_flat = eval(points)
+    Z = Z_flat.reshape(X1.shape)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Plot the surface
-surf = ax.plot_surface(X1, X2, Z, cmap='viridis', alpha=0.7)
-
-# Function to update the plot for each frame
-def update(num, positions, valuations, ax):
-    ax.clear()
-    ax.plot_surface(X1, X2, Z, cmap='viridis', alpha=0.7)
-    # Extract the positions for the current timestep
-    current_positions = positions[num]
-    current_valuations = valuations[num]
-    x1s = current_positions[:, 0]
-    x2s = current_positions[:, 1]
-    zs = current_valuations
-    ax.scatter(x1s, x2s, zs, color='r')  # Plot current particle positions
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
     ax.set_xlim([-100, 100])
     ax.set_ylim([-100, 100])
-    ax.set_zlim([-1000, 1000])
+    ax.set_zlim([-1000, 0])
 
-# Creating the Animation object
-ani = animation.FuncAnimation(fig, update, frames=range(positions.shape[0]), fargs=(positions, valuations, ax))
+    # Plot the surface
+    surf = ax.plot_surface(X1, X2, Z, cmap='viridis', alpha=0.7)
 
-plt.show()
+    # Function to update the plot for each frame
+    def update(frameId, ax, positions, valuations):
+        ax.clear()
+        ax.plot_surface(X1, X2, Z, cmap='viridis', alpha=0.7)
+
+        # Extract the positions for the current timestep
+        current_positions = positions[frameId]
+        x1s = current_positions[:, 0]
+        x2s = current_positions[:, 1]
+
+        # Extract the valuations for the current timestep
+        current_valuations = valuations[frameId]
+        zs = current_valuations
+
+        ax.scatter(x1s, x2s, zs, color='r')  # Plot current particle positions
+
+    # Creating the Animation object
+    # frames = range(positions.shape[0])
+    frames = len(range(positions.shape[0]))
+    anim = animation.FuncAnimation(fig, update, frames=frames, fargs=(ax, positions, valuations), repeat=False)
+
+    anim.save(animation_path, writer=FFMpegWriter(fps=10))
+    plt.show()
+
+animate_episode(positions, valuations)
