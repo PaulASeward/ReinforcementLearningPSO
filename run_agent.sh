0850d0f7353805d67c4adfe876914de17fde10d9
Paul <pseward@upei.ca>
2024-05-21 09:51:51 -0300

    update run_agent script

--- run_agent.sh
+++ run_agent.sh
@@ -3,8 +3,8 @@
 #SBATCH --cpus-per-task=2         # CPU cores/threads
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --gpus-per-node=1
-#SBATCH --time=159:00:00
-#SBATCH --job-name=f6_drqn_random
+#SBATCH --time=159:59:59
+#SBATCH --job-name=f6_drqn_5_actions
 #SBATCH --output=%x-%j.out
 source ~/scratch/TF_RL/bin/activate
-python main.py --network_type=DRQN --func_num=6 --num_actions=3 --num_episodes=20 --num_swarm_obs_intervals=10 --swarm_obs_interval_length=30
+python main.py --network_type=DRQN --func_num=6 --num_actions=5 --num_episodes=20 --num_swarm_obs_intervals=10 --swarm_obs_interval_length=30
