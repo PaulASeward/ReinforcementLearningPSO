0f8aeedd92272a06aa32ea0c45360f32110f8194
Paul <pseward@upei.ca>
2024-07-20 18:13:56 -0300

    add actions to seperate file

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     parser.add_argument("--num_episodes", type=int, default=20, help="The number of episodes in each Reinforcement Learning Iterations before terminating.")
     parser.add_argument("--num_swarm_obs_intervals", type=int, default=10, help="The number of swarm observation intervals. Ex) At 10 evenly spaced observation intervals, observations in the swarm will be collected. Default is 10.")
     parser.add_argument("--swarm_obs_interval_length", type=int, default=30, help="The number of observations per episode conducted in the swarm. Ex) Particle Best Replacement Counts are averaged over the last _ observations before an episode terminates and action is decided. Default is 30.")
-    parser.add_argument("--train", type=bool, default=False, help="Whether to train a network or to examine a given network")
-    parser.add_argument("--steps", type=int, default=20000, help="number of iterations to train")
+    parser.add_argument("--train", type=bool, default=True, help="Whether to train a network or to examine a given network")
+    parser.add_argument("--steps", type=int, default=2000, help="number of iterations to train")
     args, remaining = parser.parse_known_args()
 
     if args.algorithm == "PSO":
