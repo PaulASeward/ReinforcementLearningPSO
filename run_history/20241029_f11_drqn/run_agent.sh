e8cadc5a712c2addbb6d1d5bf0277fa26662f60d
Paul <pseward@upei.ca>
2024-10-29 14:10:23 -0300

    run hyperparam exp on f11 dqn drqn

--- run_history/20241029_f11_drqn/run_agent.sh
+++ run_history/20241029_f11_drqn/run_agent.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --gpus-per-node=1
 #SBATCH --time=159:59:59
-#SBATCH --job-name=f11_dqn_hyperparam
+#SBATCH --job-name=f11_drqn_hyperparam
 #SBATCH --output=%x-%j.out
 source ~/scratch/TF_RL/bin/activate
-python main.py --network_type=DQN --func_num=11 --num_actions=5 --num_episodes=20 --num_swarm_obs_intervals=10 --swarm_obs_interval_length=30
+python main.py --network_type=DRQN --func_num=11 --num_actions=5 --num_episodes=20 --num_swarm_obs_intervals=10 --swarm_obs_interval_length=30
