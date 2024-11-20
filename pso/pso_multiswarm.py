0134466dd9aa38e66782a5ac760ff64e3f84bae6
Paul <pseward@upei.ca>
2024-11-20 10:52:57 -0400

    swapping logic

--- pso/pso_multiswarm.py
+++ pso/pso_multiswarm.py
@@ -10,7 +10,8 @@ class PSOMultiSwarm:
         self.objective_function = objective_function
         # clone config to avoid changing the original config
         self.sub_swarm_config = config.clone()
-        self.sub_swarm_config.swarm_size = config.swarm_size // config.num_sub_swarms
+        self.sub_swarm_size = config.swarm_size // config.num_sub_swarms
+        self.sub_swarm_config.swarm_size = self.sub_swarm_size
         self.sub_swarm_config.is_sub_swarm = True
         self.dim = config.dim
         self.num_sub_swarms = config.num_sub_swarms
@@ -49,10 +50,80 @@ class PSOMultiSwarm:
         self.gbest_val = self.sub_swarms[best_sub_swarm_idx].gbest_val
         self.gbest_pos = self.sub_swarms[best_sub_swarm_idx].gbest_pos
 
+    def reorganize_swarms(self):
+        # Reorganize particles into subswarms, grouped by relative fitness.
+
+        # Call each sub_swarm to return a value of their p_best fitness.
+        P_vals = np.array([sub_swarm.P_vals for sub_swarm in self.sub_swarms])
+
+        # Make a one-dimensional array of all p_best values
+        pbest_vals_flattened = P_vals.flatten()
+
+        # Generate a list representing the rank of each particles pbest value in the global swarm
+        global_swarm_rank_indices = np.argsort(pbest_vals_flattened)
+
+        sub_swarm_rank_indices = global_swarm_rank_indices.reshape((self.num_sub_swarms, self.sub_swarm_size))
+
+        range_per_sub_swarm = [(i*self.sub_swarm_size, (i+1)*self.sub_swarm_size) for i in range(self.num_sub_swarms)]
+
+        # Determine the target subswarm for each particle
+        sub_swarm_targets = global_swarm_rank_indices // self.sub_swarm_size
+
+        # Identify particles to be swapped
+        particles_to_displace = [set() for _ in range(self.num_sub_swarms)]
+
+        for global_index, target_sub_swarm_idx in enumerate(sub_swarm_targets):
+            current_sub_swarm_idx = global_index // self.sub_swarm_size
+            current_idx_within_sub_swarm = global_index % self.sub_swarm_size
+
+            if current_sub_swarm_idx != target_sub_swarm_idx:
+                # Add particle to the set of swaps
+                particle = self.sub_swarms[current_sub_swarm_idx].get_particle(current_idx_within_sub_swarm)
+                particles_to_displace[current_sub_swarm_idx].add((
+                    particle,
+                    current_sub_swarm_idx,
+                    current_idx_within_sub_swarm,
+                    target_sub_swarm_idx
+                ))
+
+        # Start with the first particle to swap in to a target sub_swarm
+        incoming_particle_meta_data = None
+        for subswarm_particles_to_displace in particles_to_displace:
+            if subswarm_particles_to_displace:
+                incoming_particle_meta_data = subswarm_particles_to_displace.pop()
+                subswarm_particles_to_displace.add(incoming_particle_meta_data)
+                break
+
+
+        # Perform the swaps
+        while incoming_particle_meta_data is not None:
+            incoming_particle, current_sub_swarm_idx, current_idx_within_sub_swarm, target_sub_swarm_idx = incoming_particle_meta_data
+
+            if len(particles_to_displace[target_sub_swarm_idx]) > 0:
+
+                target_particle, target_sub_swarm_idx, target_current_idx_within_sub_swarm, target_target_sub_swarm_idx = particles_to_displace[target_sub_swarm_idx].pop()
+                self.sub_swarms[target_sub_swarm_idx].add_particle(incoming_particle, target_current_idx_within_sub_swarm)
+
+                incoming_particle_meta_data = (target_particle, target_sub_swarm_idx, target_current_idx_within_sub_swarm, target_target_sub_swarm_idx)
+            else:
+                incoming_particle_meta_data = None
+
+
+
+
+
+    def swap_particle_out_of_target_subswarm(self, incoming_particle, target_sub_swarm_idx, particle_to_displace_idx):
+        displaced_particle = self.sub_swarms[target_sub_swarm_idx].get_particle(particle_to_displace_idx)
+        self.sub_swarms[target_sub_swarm_idx].add_particle(incoming_particle, particle_to_displace_idx)
+
+        return displaced_particle
+
+
     def optimize(self):
         for sub_swarm in self.sub_swarms:
             sub_swarm.optimize()
         self.update_gbest()
+        self.reorganize_swarms()
 
 
 
--- pso/pso_swarm.py
+++ pso/pso_swarm.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+class Particle:
+    def __init__(self, x, v, p, P_val, current_valuation, velocity_magnitude, relative_fitness, average_batch_count):
+        self.x = x
+        self.v = v
+        self.p = p
+        self.P_val = P_val
+        self.velocity_magnitude = velocity_magnitude
+        self.current_valuation = current_valuation
+        self.relative_fitness = relative_fitness
+        self.average_batch_count = average_batch_count
+
+
+
 
 class PSOSwarm:
 
@@ -32,14 +45,14 @@ class PSOSwarm:
         self.abs_max_position = self.rangeF
 
         self.velocity_magnitudes = None
-        self.relative_fitness = None
+        self.relative_fitnesses = None
         self.average_batch_counts = None
         self.pbest_replacement_counts = None
 
-        self.val = None
+        self.P_vals = None
+        self.current_valuations = None
         self.gbest_val = None
         self.gbest_pos = None
-        self.pbest_val = None
 
         # Initialize the swarm's positions velocities and best solutions
         self._initialize()
@@ -52,6 +65,7 @@ class PSOSwarm:
         self.X = np.random.uniform(low=-1 * self.rangeF, high=self.rangeF, size=(self.swarm_size, self.dimension))
         self.V = np.full((self.swarm_size, self.dimension), 0)
         self.P = self.X
+        self.P_vals = None
 
         # Reset the adjustable parameters to starting values
         self.w = self.config.w
@@ -67,28 +81,28 @@ class PSOSwarm:
 
     def initialize_stored_counts(self):
         self.velocity_magnitudes = None
-        self.relative_fitness = None
-        self.val = None
+        self.relative_fitnesses = None
+
+        self.current_valuations = None
         self.gbest_val = None
         self.gbest_pos = None
-        self.pbest_val = None
 
-        # Static class variables to track pbest_val replacements
+        # Static class variables to track P_vals replacements
         self.pbest_replacement_counts = np.zeros(self.swarm_size)
         self.pbest_replacement_batchcounts = np.zeros((self.num_swarm_obs_intervals, self.swarm_size))
         self.average_batch_counts = np.zeros(self.swarm_size)
 
     def update_swarm_valuations_and_bests(self):
-        self.val = self.eval(self.P)  # Vector of particle's current value of its position based on Function Eval
-        self.pbest_val = self.val  # Vector of particle's best value of a visited position.
-        self.gbest_pos = self.P[np.argmin(self.pbest_val)]  # Vector of globally best visited position.
-        self.gbest_val = np.min(self.pbest_val)  # Single value of the global best position
+        self.P_vals = self.eval(self.P)  # Vector of particle's current valuation of its best position based on Function Eval
+        self.current_valuations = self.eval(self.P)  # Vector of particle's current value of its position based on Function Eval
+        self.gbest_pos = self.P[np.argmin(self.P_vals)]  # Vector of globally best visited position.
+        self.gbest_val = np.min(self.P_vals)  # Single valuation of the global best position
 
-        self.update_relative_fitness()
+        self.update_relative_fitnesses()
         self.update_velocity_maginitude()
 
-    def update_relative_fitness(self):
-        self.relative_fitness = (self.pbest_val - self.gbest_val) / np.abs(self.pbest_val)
+    def update_relative_fitnesses(self):
+        self.relative_fitnesses = (self.P_vals - self.gbest_val) / np.abs(self.P_vals)
 
     def update_velocity_maginitude(self):
         self.velocity_magnitudes = np.linalg.norm(self.V, axis=1)
@@ -98,14 +112,17 @@ class PSOSwarm:
         self.average_batch_counts = np.mean(self.pbest_replacement_batchcounts, axis=0)
 
     def get_observation(self):
-        self.update_relative_fitness()
+        self.update_relative_fitnesses()
         self.update_velocity_maginitude()
         self.update_batch_counts()
 
-        return np.concatenate([self.velocity_magnitudes, self.relative_fitness, self.average_batch_counts], axis=0)
+        return np.concatenate([self.velocity_magnitudes, self.relative_fitnesses, self.average_batch_counts], axis=0)
 
     def get_current_best_fitness(self):
         return self.gbest_val
+    
+    def get_current_best_fitnesses(self):
+        return self.P_vals
 
     def eval(self, X):
         return self.function.Y_matrix(np.array(X).astype(float))
@@ -131,18 +148,18 @@ class PSOSwarm:
         self.V = np.where(out_of_bounds, 0, self.V)  # Out-of-bounds velocities are set to 0
 
         # Use function evaluation for each particle (vector) in Swarm to provide value for each position in X.
-        self.val = self.eval(self.X)
+        self.current_valuations = self.eval(self.X)
 
     def update_pbest(self):
-        improved_particles = self.val < self.pbest_val  # Update each Particle's best position for each particle index
+        improved_particles = self.current_valuations < self.P_vals  # Update each Particle's best position for each particle index
         self.P = np.where(improved_particles[:, np.newaxis], self.X, self.P)
-        self.pbest_val = np.where(improved_particles, self.val, self.pbest_val)
+        self.P_vals = np.where(improved_particles, self.current_valuations, self.P_vals)
 
-        self.pbest_replacement_counts += improved_particles  # Update pbest_val replacement counter
+        self.pbest_replacement_counts += improved_particles  # Update P_vals replacement counter
 
     def update_gbest(self):
-        self.gbest_pos = self.P[np.argmin(self.pbest_val)]
-        self.gbest_val = np.min(self.pbest_val)
+        self.gbest_pos = self.P[np.argmin(self.P_vals)]
+        self.gbest_val = np.min(self.P_vals)
 
     def optimize(self):
         for obs_interval_idx in range(self.num_swarm_obs_intervals):
@@ -154,6 +171,26 @@ class PSOSwarm:
 
             self.pbest_replacement_batchcounts[obs_interval_idx] = self.pbest_replacement_counts
             self.pbest_replacement_counts = np.zeros(self.swarm_size)
+
+    def get_particle(self, particle_index: int):
+        return Particle(self.X[particle_index],
+                        self.V[particle_index],
+                        self.P[particle_index],
+                        self.P_vals[particle_index],
+                        self.current_valuations[particle_index],
+                        self.velocity_magnitudes[particle_index],
+                        self.relative_fitnesses[particle_index],
+                        self.average_batch_counts[particle_index])
+
+    def add_particle(self, particle: Particle, particle_index: int):
+        self.X[particle_index] = particle.x
+        self.V[particle_index] = particle.v
+        self.P[particle_index] = particle.p
+        self.P_vals[particle_index] = particle.P_val
+        self.current_valuations[particle_index] = particle.current_valuation
+        self.velocity_magnitudes[particle_index] = particle.velocity_magnitude
+        self.relative_fitnesses[particle_index] = particle.relative_fitness
+        self.average_batch_counts[particle_index] = particle.average_batch_count
 
 
 
