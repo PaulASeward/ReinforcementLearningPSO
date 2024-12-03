012a2d5be6c5e7cfd7e3c0faad5d52c5ac23c4de
Paul <pseward@upei.ca>
2024-12-03 13:57:06 -0400

    remove old env

--- agents/drqn_agent.py
+++ agents/drqn_agent.py
@@ -1,11 +1,7 @@
 from agents.agent import BaseAgent
-from environment.mock.mock_env import MockEnv
-from environment.env import PSOEnv
-from tf_agents.environments import tf_py_environment
 import gymnasium as gym
 
 import numpy as np
-import tensorflow as tf
 from agents.utils.experience_buffer import ExperienceBufferRecurrent as ReplayBuffer
 from agents.model_networks.drqn_model import DRQNModel
 from utils.logging_utils import DiscreteActionsResultsLogger as ResultsLogger
@@ -31,15 +27,6 @@ class DRQNAgent(BaseAgent):
             self.env = self.raw_env
 
         return self.env
-    # def build_environment(self):
-    #     if self.config.use_mock_data:
-    #         self.raw_env = MockEnv(self.config)  # Mock environment
-    #         self.env = tf_py_environment.TFPyEnvironment(self.raw_env)  # Training environment
-    #     else:
-    #         self.raw_env = PSOEnv(self.config)  # Raw environment
-    #         self.env = tf_py_environment.TFPyEnvironment(self.raw_env)  # Training environment
-    #
-    #     return self.env
 
     def update_states(self, next_state):
         self.states = np.roll(self.states, -1, axis=0)
@@ -48,14 +35,11 @@ class DRQNAgent(BaseAgent):
     def train(self):
         with self.writer.as_default():
             for ep in range(self.config.train_steps):
-                terminal = False
-                actions, rewards = [], []
+                actions, rewards, swarm_observations, terminal = [], [], [], False
 
                 self.states = np.zeros([self.config.trace_length, self.config.observation_length])  # Starts with choosing an action from empty states. Uses rolling window size 4
-                # current_state = self.env.reset()
                 observation, swarm_info = self.env.reset()
 
-                # self.update_states(current_state.observation)  # Check states array update
                 self.update_states(observation)  # Check states array update
 
                 while not terminal:
@@ -63,12 +47,8 @@ class DRQNAgent(BaseAgent):
                     q_values = self.model.get_action_q_values(input_state)
                     action = self.policy.select_action(q_values)
 
-                    next_observation, reward, terminal, info = self.env.step(action)
-                    next_state = np.reshape(next_observation, [1, self.config.trace_length, self.config.observation_length])
-
-                    # step_type, reward, discount, next_state = self.env.step(action)
-                    # reward = reward.numpy()[0]
-                    # terminal = bool(1 - discount)  # done is 0 (not done) if discount=1.0, and 1 if discount = 0.0
+                    next_state, reward, terminal, swarm_info = self.env.step(action)
+                    # next_state = np.reshape(next_observation, [1, self.config.trace_length, self.config.observation_length])
 
                     prev_states = self.states
                     self.update_states(next_state)  # Updates the states array removing oldest when adding newest for sliding window
@@ -76,6 +56,8 @@ class DRQNAgent(BaseAgent):
 
                     actions.append(action)
                     rewards.append(reward)
+                    swarm_observations.append(swarm_info)
+
 
                 losses = None
                 if self.replay_buffer.size() >= self.config.batch_size:
@@ -83,5 +65,5 @@ class DRQNAgent(BaseAgent):
 
                 self.update_model_target_weights()  # target model gets updated AFTER episode, not during like the regular model.
 
-                self.results_logger.save_log_statements(step=ep+1, actions=actions, rewards=rewards, train_loss=losses, epsilon=self.policy.current_epsilon)
+                self.results_logger.save_log_statements(step=ep+1, actions=actions, rewards=rewards, train_loss=losses, epsilon=self.policy.current_epsilon, swarm_observations=swarm_observations)
             self.results_logger.print_execution_time()
--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -8,38 +8,65 @@ from utils.plot_utils import plot_standard_results, plot_continuous_actions, plo
 class ResultsLogger:
     def __init__(self, config):
         self.config = config
+        self.step = 0
+        self.train_steps = config.train_steps
+        self.num_episodes = config.num_episodes
         self.start_time = time.time()
+        self.last_log_interval_time = self.start_time
 
-        self.loss = []
-        self.returns = []
-        self.fitness = []
+        self.average_loss = []
+        self.average_returns = []
+        self.average_fitness = []
+
+        # Initialize the results with to be stored
+        self.swarm_episode_observations = np.empty((0, 20), dtype=object)  # NumPy array for better handling
         self.eval_interval_count = 0
+
+        self.save_config()
 
     def plot_results(self):
         raise NotImplementedError
 
-    def save_actions(self, step, actions_row):
+    def save_actions(self, actions_row):
         raise NotImplementedError
+
+    def save_config(self):
+        with open(self.config.experiment_config_path, 'w') as f:
+            for key, value in vars(self.config).items():
+                f.write(f"{key}: {value}\n")
+
+    def save_step_results(self, epsilon, rewards, train_loss=None, swarm_observations_dicts=None):
+        if train_loss is None:
+            train_loss = 0.0
+        else:
+            train_loss = np.mean(train_loss)
+
+        cumulative_episode_reward = np.sum(rewards)
+        fitness = self.config.fDeltas[self.config.func_num - 1] - cumulative_episode_reward
+
+        self._save_to_csv(rewards, self.config.action_values_path)
+        swarm_observations = np.array([swarm_observations_dicts], dtype=object)  # Convert dict to array
+        # self.swarm_episode_observations = np.vstack([self.swarm_episode_observations, swarm_observations])
+
+        self._save_to_csv([epsilon], self.config.epsilon_values_path)  # TODO: remove
+        self._save_to_csv([self.step, epsilon, cumulative_episode_reward, fitness, train_loss], self.config.training_step_results_path)
+
+        print(f"Step #{self.step} Reward:{cumulative_episode_reward} Current Epsilon: {epsilon}")
+        tf.summary.scalar("episode_reward", cumulative_episode_reward, step=self.step-1)
 
     def write_actions_at_eval_interval_to_csv(self):
         raise NotImplementedError
 
     def save_log_statements(self, step, actions, rewards, train_loss=None, epsilon=None, swarm_observations=None):
-        self.save_actions(step, actions)
-        self._save_to_csv(rewards, self.config.action_values_path)
-        self._save_to_csv([epsilon], self.config.epsilon_values_path)
-
-        cumulative_episode_reward = np.sum(rewards)
-        print(f"Step #{step} Reward:{cumulative_episode_reward} Current Epsilon: {epsilon}")
-        tf.summary.scalar("episode_reward", cumulative_episode_reward, step=step-1)
+        self.step = step
+        self.save_actions(actions)
+        self.save_step_results(epsilon, rewards, train_loss, swarm_observations)
 
         if step % self.config.log_interval == 0:
-            train_loss = self.store_results_at_log_interval(train_loss)
-            print('step = {0}: loss = {1}'.format(step, train_loss))
+            self.store_results_at_log_interval(train_loss)
 
         if step % self.config.eval_interval == 0:
-            avg_return, avg_fitness = self.store_results_at_eval_interval()
-            print('step = {0}: Average Return = {1} Average Fitness = {2}'.format(step, avg_return, avg_fitness))
+            self.store_results_at_eval_interval()
 
     def print_execution_time(self):
         print(f"--- Execution took {(time.time() - self.start_time) / 3600} hours ---")
@@ -49,9 +76,18 @@ class ResultsLogger:
             train_loss = 0.0
         else:
             train_loss = np.mean(train_loss)
-        self.loss.append(train_loss)
-        np.savetxt(self.config.loss_file, self.loss, delimiter=", ", fmt='% s')
-        return train_loss
+
+        self.average_loss.append(train_loss)
+        np.savetxt(self.config.loss_file, self.average_loss, delimiter=", ", fmt='% s')
+
+        # Calculate average episode time
+        current_time = time.time()
+        log_interval_run_time = current_time - self.last_log_interval_time
+        self.last_log_interval_time = current_time
+        average_episode_time = log_interval_run_time / self.config.log_interval
+
+        print(f"Step #{self.step} Loss:{train_loss} Average Episode Time: {average_episode_time / 60} minutes")
+        # print('step = {0}: loss = {1}'.format(step, train_loss))
 
     def store_results_at_eval_interval(self):
         # Read the last eval_interval number of rows to calculate the total return per row. This can be used to then calculate the relative fitness and then compute averages.
@@ -63,16 +99,16 @@ class ResultsLogger:
         avg_return = np.mean(reward_sums)  # Total return of all episodes for an iteration
         avg_fitness = np.mean(fitness)  # Furthest minimum value explored for an iteration
 
-        self.returns.append(avg_return)
-        self.fitness.append(avg_fitness)
+        self.average_returns.append(avg_return)
+        self.average_fitness.append(avg_fitness)
 
         self.write_actions_at_eval_interval_to_csv()
         self.eval_interval_count += 1
 
-        np.savetxt(self.config.average_returns_path, self.returns, delimiter=", ", fmt='% s')
-        np.savetxt(self.config.fitness_path, self.fitness, delimiter=", ", fmt='% s')
+        np.savetxt(self.config.average_returns_path, self.average_returns, delimiter=", ", fmt='% s')
+        np.savetxt(self.config.fitness_path, self.average_fitness, delimiter=", ", fmt='% s')
 
-        return avg_return, avg_fitness
+        print('step = {0}: Average Return = {1} Average Fitness = {2}'.format(self.step, avg_return, avg_fitness))
 
     def _save_to_numpy(self, path, data):
         np.save(path, data)
@@ -87,7 +123,7 @@ class DiscreteActionsResultsLogger(ResultsLogger):
         super().__init__(config)
         self.action_counts = np.zeros((self.config.num_eval_intervals, self.config.num_actions), dtype=np.int32)
 
-    def save_actions(self, step, actions_row):
+    def save_actions(self, actions_row):
         for action in actions_row:
             self.action_counts[self.eval_interval_count, action] += 1
 
@@ -106,12 +142,12 @@ class ContinuousActionsResultsLogger(ResultsLogger):
         super().__init__(config)
         self.continuous_action_history = np.zeros((self.config.train_steps, self.config.num_episodes, self.config.action_dimensions), dtype=np.float32)
 
-    def save_actions(self, step, actions_row):
+    def save_actions(self, actions_row):
         for episode_idx, action in enumerate(actions_row):
-            self.continuous_action_history[step-1, episode_idx] = action
+            self.continuous_action_history[self.step-1, episode_idx] = action
 
         # Save to disk periodically
-        if step % self.config.log_interval == 0 or step == self.config.train_steps:
+        if self.step % self.config.log_interval == 0 or self.step == self.config.train_steps:
             self._save_to_numpy(self.config.continuous_action_history_path, self.continuous_action_history)
 
     def write_actions_at_eval_interval_to_csv(self):
