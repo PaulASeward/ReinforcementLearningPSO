106ad5fa107b0897163bc3e4e56831c5c660cb29
Paul <pseward@upei.ca>
2025-01-28 15:51:40 -0400

    priority replay

--- agents/ddpg_agent.py
+++ agents/ddpg_agent.py
@@ -1,11 +1,14 @@
 from agents.agent import BaseAgent
 import numpy as np
+import tensorflow as tf
+
 from environment.gym_env_continuous import ContinuousPsoGymEnv
 from environment.gym_multiswarm_env_continuous import ContinuousMultiSwarmPsoGymEnv
 from tf_agents.environments import tf_py_environment
 
 import gymnasium as gym
-from agents.utils.experience_buffer import ExperienceBufferStandard as ReplayBuffer
+from agents.utils.experience_buffer import ExperienceBufferStandard as StandardReplayBuffer
+from agents.utils.experience_buffer import ExperienceBufferPriority as PriorityReplayBuffer
 from agents.model_networks.ddpg_model import ActorNetworkModel, CriticNetworkModel
 from utils.logging_utils import ContinuousActionsResultsLogger as ResultsLogger
 from agents.utils.policy import OrnsteinUhlenbeckActionNoisePolicy, OrnsteinUhlenbeckActionNoisePolicyWithDecayScaling
@@ -26,7 +29,7 @@ class DDPGAgent(BaseAgent):
         self.policy = OrnsteinUhlenbeckActionNoisePolicyWithDecayScaling(config)
 
         self.update_model_target_weights(tau=1.0)
-        self.replay_buffer = ReplayBuffer()
+        self.replay_buffer = PriorityReplayBuffer(config=config) if config.use_priority_replay else StandardReplayBuffer(config=config)
 
     def build_environment(self):
         if self.config.swarm_algorithm == "PMSO":
@@ -84,40 +87,58 @@ class DDPGAgent(BaseAgent):
             self.actor_network_target.model.set_weights(theta_a_targ)
             self.critic_network_target.model.set_weights(theta_c_targ)
 
-    def replay_experience(self, experience_length=5):
+    def replay_experience(self):
         if self.replay_buffer.size() < self.config.batch_size:
-            return None  # Not enough experience to replay yet.
+            return None, None, None  # Not enough experience to replay yet.
 
-        losses = []
+        actor_losses = []
+        critic_losses = []
+        total_losses = []
         if not self.config.use_mock_data:
-            for _ in range(experience_length):
-                states, actions, rewards, next_states, done = self.replay_buffer.sample(self.config.batch_size)
+            for _ in range(self.config.replay_experience_length):
+                ISWeights = None
+                tree_idx = None
+                if self.config.use_priority_replay:
+                    tree_idx, samples, ISWeights = self.replay_buffer.sample(self.config.batch_size)
+                    states, actions, rewards, next_states, dones = map(lambda x: np.array(x, dtype=np.float32), zip(*samples))
+                    states = np.squeeze(states, axis=1)  # shape becomes (batch_size, 150)
+                    next_states = np.squeeze(next_states, axis=1)
+                else:
+                    states, actions, rewards, next_states, dones = self.replay_buffer.sample(self.config.batch_size)
 
-                # ---------------------------- update critic ---------------------------- #
+
                 next_actions = self.actor_network_target.predict(next_states)
                 next_q_values = self.critic_network_target.predict([next_states, next_actions])
 
                 # Use Bellman Equation. (recursive definition of q-values)
-                q_values_targets = rewards + (1 - done) * self.config.gamma * next_q_values
+                q_values_targets = rewards + (1 - dones) * self.config.gamma * next_q_values
                 q_values_targets = q_values_targets.astype(np.float32)
 
-                critic_loss = self.critic_network.train(states, actions, q_values_targets)
+                # ---------------------------- update critic ---------------------------- #
+                critic_loss, td_error = self.critic_network.train(states, actions, q_values_targets, ISWeights)
+
+                # update priority buffer
+                if self.config.use_priority_replay:
+                    abs_errors = tf.reduce_sum(tf.abs(td_error), axis=1)
+                    self.replay_buffer.batch_update(tree_idx, abs_errors)
 
                 # ---------------------------- update actor ---------------------------- #
-                action_loss = self.actor_network.train(states, self.critic_network.model)
+                actor_loss = self.actor_network.train(states, self.critic_network.model)
 
-                losses.append(action_loss + critic_loss)
+                total_losses.append(actor_loss + critic_loss)
+                actor_losses.append(actor_loss)
+                critic_losses.append(critic_loss)
 
-        return losses
+        return total_losses, actor_losses, critic_losses
 
 
-    # def replay_experience(self, experience_length=10):
+    # def replay_experience(self):
     #     if self.replay_buffer.size() < self.config.batch_size:
     #         return None  # Not enough experience to replay yet.
     #
     #     losses = []
     #     if not self.config.use_mock_data:
-    #         for _ in range(experience_length):  # Why size 10?
+    #         for _ in range(self.config.replay_experience_length):
     #             states, actions, rewards, next_states, done = self.replay_buffer.sample(self.config.batch_size)
     #
     #             # ---------------------------- update critic ---------------------------- #
