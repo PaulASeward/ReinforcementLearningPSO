89866f45c98a7194e9928edb6ea6d34a757107d8
Paul <pseward@upei.ca>
2025-02-20 11:57:10 -0400

    stage run_history exp

---
+++ run_history/20250220_f11_difference_reward/run_agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-bolufe
+#SBATCH --cpus-per-task=2         # CPU cores/threads
+#SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
+#SBATCH --gpus-per-node=1
+#SBATCH --time=159:59:59
+#SBATCH --job-name=f11_ddpg
+#SBATCH --output=%x-%j.out
+source ~/scratch/TF_RL/bin/activate
+python main.py --network_type=DDPG --swarm_algorithm=PSO --func_num=11 --action_dimensions=3 --num_episodes=20 --num_swarm_obs_intervals=10 --swarm_obs_interval_length=30
